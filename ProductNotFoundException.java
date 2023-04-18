eff80eee6d1931108852f62baa04ee3a12e77ade
Shwetayadav123 <116827484+Shwetayadav123@users.noreply.github.com>
2023-04-18 19:17:45 +0530

    Delete ProductNotFoundException.java

--- ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	public ProductNotFoundException(String message) {
-		super(message);
-	}
-}
